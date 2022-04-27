0928de893ebe92851cd4bf6ad100d25e8779fda9
sahar sakhaiee <100339904+shrsakhaiee@users.noreply.github.com>
2022-04-27 14:54:37 +0430

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿
+namespace Tic_Tac_Toe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.Btn_Restart = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.AutoSize = true;
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tableLayoutPanel1.Controls.Add(this.Btn_Restart, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 2, 3);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(924, 443);
+            this.tableLayoutPanel1.TabIndex = 10;
+            // 
+            // Btn_Restart
+            // 
+            this.Btn_Restart.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Btn_Restart.AutoSize = true;
+            this.Btn_Restart.BackColor = System.Drawing.Color.HotPink;
+            this.Btn_Restart.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.Btn_Restart.ForeColor = System.Drawing.Color.Black;
+            this.Btn_Restart.Location = new System.Drawing.Point(3, 333);
+            this.Btn_Restart.Name = "Btn_Restart";
+            this.Btn_Restart.Size = new System.Drawing.Size(301, 107);
+            this.Btn_Restart.TabIndex = 11;
+            this.Btn_Restart.Text = "restart";
+            this.Btn_Restart.UseVisualStyleBackColor = false;
+            this.Btn_Restart.Click += new System.EventHandler(this.Btn_Restart_Click_1);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.AutoSize = true;
+            this.button1.BackColor = System.Drawing.Color.HotPink;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(618, 333);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(303, 107);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Exit";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(948, 467);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button Btn_Restart;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tic_Tac_Toe
+{
+    public partial class Form1 : Form
+    {
+        Button[,] dokmeha;
+        int ply;
+        public Form1()
+        {
+            InitializeComponent();
+
+             dokmeha = new Button[3, 3] ;
+             s();
+
+             new_game();
+    
+        }
+
+        private void s()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    dokmeha[i, j] = new Button();
+                    dokmeha[i, j].Click += new EventHandler(button1_Click);
+                    dokmeha[i, j].Font = new Font("",40);
+                    dokmeha[i, j].Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Right | AnchorStyles.Left;
+                    tableLayoutPanel1.Controls.Add(dokmeha[i, j], i, j);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Button this_button = (sender as Button);
+            if (this_button.Text == "")
+            {
+                if (ply == 1)
+                {
+                    this_button.Text = "X";
+                    this_button.BackColor = Color.HotPink;
+                    this_button.ForeColor = Color.Pink;
+                    ply = 2;
+                }
+                else if (ply == 2)
+                {
+                    this_button.Text = "O";
+                    this_button.BackColor = Color.DarkGray;
+                    this_button.ForeColor = Color.Gray;
+                    ply = 1;
+                }
+
+                check_game();
+            }
+        }
+        private void button9_MouseHover(object sender, EventArgs e)
+        {
+            //Button this_button = (sender as Button);
+            //this_button.BackColor = Color.DarkBlue;
+        }
+
+        private void button9_MouseLeave(object sender, EventArgs e)
+        {
+           // Button this_button = (sender as Button);
+           // this_button.BackColor = Color.SkyBlue;
+        }
+
+        private void new_game()
+        {
+            ply = 1;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    dokmeha[i, j].Text = "";
+                    dokmeha[i, j].BackColor = Color.SkyBlue;
+                }
+            }
+        }
+
+        private void check_game()
+        {
+            if(dokmeha[0,0].Text=="X" && dokmeha[0, 1].Text == "X" && dokmeha[0, 2].Text == "X")
+            {
+                MessageBox.Show("بازیکن شماره یک برنده شد");
+                new_game();
+            }
+            if (dokmeha[1, 0].Text == "X" && dokmeha[1, 1].Text == "X" && dokmeha[1, 2].Text == "X")
+            {
+                MessageBox.Show("بازیکن شماره یک برنده شد");
+                new_game();
+            }
+            if (dokmeha[2, 0].Text == "X" && dokmeha[2, 1].Text == "X" && dokmeha[2, 2].Text == "X")
+            {
+                MessageBox.Show("بازیکن شماره یک برنده شد");
+                new_game();
+            }
+            if (dokmeha[0, 0].Text == "X" && dokmeha[1, 0].Text == "X" && dokmeha[2, 0].Text == "X")
+            {
+                MessageBox.Show("بازیکن شماره یک برنده شد");
+                new_game();
+            }
+            if (dokmeha[0, 1].Text == "X" && dokmeha[1, 1].Text == "X" && dokmeha[2, 1].Text == "X")
+            {
+                MessageBox.Show("بازیکن شماره یک برنده شد");
+                new_game();
+            }
+            if (dokmeha[0, 2].Text == "X" && dokmeha[1, 2].Text == "X" && dokmeha[2, 2].Text == "X")
+            {
+                MessageBox.Show("بازیکن شماره یک برنده شد");
+                new_game();
+            }
+            if (dokmeha[0, 0].Text == "X" && dokmeha[1, 1].Text == "X" && dokmeha[2, 2].Text == "X")
+            {
+                MessageBox.Show("بازیکن شماره یک برنده شد");
+                new_game();
+            }
+            if (dokmeha[0, 2].Text == "X" && dokmeha[1, 1].Text == "X" && dokmeha[2, 0].Text == "X")
+            {
+                MessageBox.Show("بازیکن شماره یک برنده شد");
+                new_game();
+            }
+
+
+            if (dokmeha[0, 0].Text == "O" && dokmeha[0, 1].Text == "O" && dokmeha[0, 2].Text == "O")
+            {
+                MessageBox.Show("بازیکن شماره دو برنده شد");
+                new_game();
+            }
+            if (dokmeha[1, 0].Text == "O" && dokmeha[1, 1].Text == "O" && dokmeha[1, 2].Text == "O")
+            {
+                MessageBox.Show("بازیکن شماره دو برنده شد");
+                new_game();
+            }
+            if (dokmeha[2, 0].Text == "O" && dokmeha[2, 1].Text == "O" && dokmeha[2, 2].Text == "O")
+            {
+                MessageBox.Show("بازیکن شماره دو برنده شد");
+                new_game();
+            }
+            if (dokmeha[0, 0].Text == "O" && dokmeha[1, 0].Text == "O" && dokmeha[2, 0].Text == "O")
+            {
+                MessageBox.Show("بازیکن شماره دو برنده شد");
+                new_game();
+            }
+            if (dokmeha[0, 1].Text == "O" && dokmeha[1, 1].Text == "O" && dokmeha[2, 1].Text == "O")
+            {
+                MessageBox.Show("بازیکن شماره دو برنده شد");
+                new_game();
+            }
+            if (dokmeha[0, 2].Text == "O" && dokmeha[1, 2].Text == "O" && dokmeha[2, 2].Text == "O")
+            {
+                MessageBox.Show("بازیکن شماره دو برنده شد");
+                new_game();
+            }
+            if (dokmeha[0, 0].Text == "O" && dokmeha[1, 1].Text == "O" && dokmeha[2, 2].Text == "O")
+            {
+                MessageBox.Show("بازیکن شماره دو برنده شد");
+                new_game();
+            }
+            if (dokmeha[0, 2].Text == "O" && dokmeha[1, 1].Text == "O" && dokmeha[2, 0].Text == "O")
+            {
+                MessageBox.Show("بازیکن شماره دو برنده شد");
+                new_game();
+            }
+            else if (dokmeha[0, 0].Text != "" && dokmeha[0, 1].Text != "" && dokmeha[0, 2].Text != "" && dokmeha[1, 0].Text != "" && dokmeha[1, 1].Text != "" && dokmeha[1, 2].Text != "" && dokmeha[2, 0].Text != "" && dokmeha[2, 1].Text != "" && dokmeha[2, 2].Text != "")
+            {
+                MessageBox.Show("مساوی شد");
+                new_game();
+            }
+        }
+
+        private void Btn_Restart_Click_1(object sender, EventArgs e)
+        {
+            new_game();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+    }
+
